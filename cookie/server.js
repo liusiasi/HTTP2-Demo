b4040a8259e5aeb6b12de433cddf8354f6a3ba5f
liusisi <liusisi@163.com>
2019-07-26 10:23:38 +0800

    http

---
+++ cookie/server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer(function (request, response) {
+  console.log('request come', request.url)
+  const host = request.headers.host;
+
+  if (request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8')
+    if( host === 'test.com'){
+      response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Set-Cookie': ['id=123; max-age=2', 'abc=456;domain=test.com']
+      })
+    }
+
+    response.end(html)
+  }
+
+}).listen(8888)
+
+console.log('server listening on 8888')
---
+++ csp/server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer(function (request, response) {
+  console.log('request come', request.url)
+
+  if (request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8')
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      // 'Content-Security-Policy-Report-Only': 'default-src \'self\' https://cdn.bootcss.com/; form-action \'self\'; report-uri /report'
+    })
+    response.end(html)
+  } else {
+    response.writeHead(200, {
+      'Content-Type': 'application/javascript'
+    })
+    response.end('console.log("loaded script")')
+  }
+}).listen(8888)
+
+console.log('server listening on 8888')
